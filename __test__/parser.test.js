02c2488154272382b0a688b4573171397d1c34f5
Sergi <solive@uic.es>
2021-05-11 13:15:18 +0200

    added es linter (airbnb-base) to lint the code & style

--- __test__/parser.test.js
+++ __test__/parser.test.js
@@ -1,98 +1,101 @@
-const { test, expect } = require("@jest/globals");
-const fs = require("fs")
-const path = require("path")
-const html = fs.readFileSync(path.resolve(__dirname, '../availability.html'), 'utf8');
-const jsdom = require("jsdom");
+/* eslint-disable linebreak-style */
+const {
+  test, expect, describe, beforeEach, it,
+} = require('@jest/globals');
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
+const jsdom = require('jsdom');
+const getSearchData = require('../parser');
+
+const html = readFileSync(resolve(__dirname, '../availability.html'), 'utf8');
 const { JSDOM } = jsdom;
-const dom = new JSDOM(html)
-const htmlBody = dom.window.document.body.innerHTML
-const getSearchData = require("../parser.js")
+const dom = new JSDOM(html);
+const htmlBody = dom.window.document.body.innerHTML;
 
-global.document = dom.window.document
-global.window = dom.window
-
+global.document = dom.window.document;
+global.window = dom.window;
 
 beforeEach(() => {
-    document.body.innerHTML = htmlBody
-    document.firstElementChild.setAttribute("lang", "es")
-  })
+  document.body.innerHTML = htmlBody;
+  document.firstElementChild.setAttribute('lang', 'es');
+});
 
-describe("Test getUserSearchData function against availability.html", () => {
-    test("when executed, the return value should be an object", () => {
-        expect(typeof getSearchData()).toBe("object")
-    })
+describe('Test getUserSearchData function against availability.html', () => {
+  test('when executed, the return value should be an object', () => {
+    expect(typeof getSearchData()).toBe('object');
+  });
 
-    describe("The returned object", () => {
-        it("should contain the check-in date", () => {
-            expect(getSearchData().checkIn).toBe("2021-06-21")
-            expect(typeof getSearchData().checkIn).toBe("string")
-        })
+  describe('The returned object', () => {
+    it('should contain the check-in date', () => {
+      expect(getSearchData().checkIn).toBe('2021-06-21');
+      expect(typeof getSearchData().checkIn).toBe('string');
+    });
 
-        it("should contain the check-out date", () => {
-            expect(getSearchData().checkOut).toBe("2021-06-25")
-            expect(typeof getSearchData().checkOut).toBe("string")
-        })
-        
-        it("should contain the minimum price", () => {
-            expect(getSearchData().minimumPrice).toBe(354.71)
-            expect(typeof getSearchData().minimumPrice).toBe("number")
-        })
+    it('should contain the check-out date', () => {
+      expect(getSearchData().checkOut).toBe('2021-06-25');
+      expect(typeof getSearchData().checkOut).toBe('string');
+    });
 
-        it('should contain the price for website members', () => {
-            expect(getSearchData().priceForMembers).toBe(301.50)
-            expect(typeof getSearchData().priceForMembers).toBe('number')
-        })
+    it('should contain the minimum price', () => {
+      expect(getSearchData().minimumPrice).toBe(354.71);
+      expect(typeof getSearchData().minimumPrice).toBe('number');
+    });
 
-        it("should contain the currency code", () => {
-            expect(getSearchData().currencyCode).toBe("EUR")
-            expect(typeof getSearchData().currencyCode).toBe("string")
-        })
+    it('should contain the price for website members', () => {
+      expect(getSearchData().priceForMembers).toBe(301.50);
+      expect(typeof getSearchData().priceForMembers).toBe('number');
+    });
 
-        it("should contain the number of rooms desired", () => {
-            expect(getSearchData().numberOfRooms).toBe(1)
-            expect(typeof getSearchData().numberOfRooms).toBe("number")
-        })
+    it('should contain the currency code', () => {
+      expect(getSearchData().currencyCode).toBe('EUR');
+      expect(typeof getSearchData().currencyCode).toBe('string');
+    });
 
-        it("should contain the number of guests (splitted by adults and children)", () => {
-            expect(getSearchData().numberOfGuests.adults).toBe(2)
-            expect(getSearchData().numberOfGuests.children).toBe(1)
-            expect(typeof getSearchData().numberOfGuests.adults).toBe("number")
-            expect(typeof getSearchData().numberOfGuests.children).toBe("number")
-        })
+    it('should contain the number of rooms desired', () => {
+      expect(getSearchData().numberOfRooms).toBe(1);
+      expect(typeof getSearchData().numberOfRooms).toBe('number');
+    });
 
-        it("should contain the total amount of guests", () => {
-            expect(getSearchData().totalAmountOfGuests).toBe(3)
-            expect(typeof getSearchData().totalAmountOfGuests).toBe("number")
-        })
+    it('should contain the number of guests (splitted by adults and children)', () => {
+      expect(getSearchData().numberOfGuests.adults).toBe(2);
+      expect(getSearchData().numberOfGuests.children).toBe(1);
+      expect(typeof getSearchData().numberOfGuests.adults).toBe('number');
+      expect(typeof getSearchData().numberOfGuests.children).toBe('number');
+    });
 
-        it("should contain the language code", () => {
-            expect(getSearchData().languageCode).toBe("ES")
-            expect(typeof getSearchData().languageCode).toBe("string")
-        })
+    it('should contain the total amount of guests', () => {
+      expect(getSearchData().totalAmountOfGuests).toBe(3);
+      expect(typeof getSearchData().totalAmountOfGuests).toBe('number');
+    });
 
-        it("should contain the room details", () => {
-            expect(getSearchData().roomDetails.breakfast).toBe("Desayuno incluido")
-            expect(getSearchData().roomDetails.refundPolicy).toBe("Cancelable, modificable")
-            expect(getSearchData().roomDetails.payment).toBe("Pagar más tarde")
-            expect(typeof getSearchData().roomDetails).toBe("object")
-        })
+    it('should contain the language code', () => {
+      expect(getSearchData().languageCode).toBe('ES');
+      expect(typeof getSearchData().languageCode).toBe('string');
+    });
 
-        it("should contain all the available rooms rates", () => {
-            expect(getSearchData().allRates.length).toBe(9)
-            expect(getSearchData().allRates[0].price).toBe(354.71)
-        })
-    })
+    it('should contain the room details', () => {
+      expect(getSearchData().roomDetails.breakfast).toBe('Desayuno incluido');
+      expect(getSearchData().roomDetails.refundPolicy).toBe('Cancelable, modificable');
+      expect(getSearchData().roomDetails.payment).toBe('Pagar más tarde');
+      expect(typeof getSearchData().roomDetails).toBe('object');
+    });
 
-    test("when DOM element not found, it should throw an error", () => {       
-        document.getElementById("fb-qs-summary-dates-arrival").firstChild.remove();
+    it('should contain all the available rooms rates', () => {
+      expect(getSearchData().allRates.length).toBe(9);
+      expect(getSearchData().allRates[0].price).toBe(354.71);
+    });
+  });
 
-        expect(getSearchData).toThrow(Error)
-        expect(getSearchData).toThrow("DATA NOT FOUND")
-    })
+  test('when DOM element not found, it should throw an error', () => {
+    document.getElementById('fb-qs-summary-dates-arrival').firstChild.remove();
 
-    test("when data is falsy, it should throw an error", () => {        
-        document.getElementById("fb-qs-summary-rooms-quantity").firstChild.setAttribute("data-mode", undefined);
-        expect(getSearchData).toThrow(Error)
-        expect(getSearchData).toThrow("DATA NOT FOUND")
-    })
-})
+    expect(getSearchData).toThrow(Error);
+    expect(getSearchData).toThrow('DATA NOT FOUND');
+  });
+
+  test('when data is falsy, it should throw an error', () => {
+    document.getElementById('fb-qs-summary-rooms-quantity').firstChild.setAttribute('data-mode', undefined);
+    expect(getSearchData).toThrow(Error);
+    expect(getSearchData).toThrow('DATA NOT FOUND');
+  });
+});
--- parser.js
+++ parser.js
@@ -1,123 +1,127 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-use-before-define */
+
 function getSearchData() {
-    try {
-        return {
-            checkIn: getCheckInDate(),
-            checkOut: getCheckOutDate(),
-            minimumPrice: getMinimumPrice(),
-            priceForMembers: getMemberPrice(),
-            currencyCode: getCurrency(),
-            numberOfRooms: getNumberOfRooms(),
-            numberOfGuests: getNumberOfGuests(),
-            totalAmountOfGuests: getTotalAmountOfGuests(),
-            languageCode: getLanguageCode(),
-            roomDetails: getRoomDetails(),
-            allRates: getAllRoomsRates()
-        };
-    } catch(error) {
-        throw new Error("DATA NOT FOUND")
-    }
+  try {
+    return {
+      checkIn: getCheckInDate(),
+      checkOut: getCheckOutDate(),
+      minimumPrice: getMinimumPrice(),
+      priceForMembers: getMemberPrice(),
+      currencyCode: getCurrency(),
+      numberOfRooms: getNumberOfRooms(),
+      numberOfGuests: getNumberOfGuests(),
+      totalAmountOfGuests: getTotalAmountOfGuests(),
+      languageCode: getLanguageCode(),
+      roomDetails: getRoomDetails(),
+      allRates: getAllRoomsRates(),
+    };
+  } catch (error) {
+    throw new Error('DATA NOT FOUND');
+  }
 }
 
 function getCheckInDate() {
-    let checkInTag = document.getElementById("fb-qs-summary-dates-arrival").firstChild;
-    return validate(checkInTag.getAttribute('data-date'))
+  const checkInTag = document.getElementById('fb-qs-summary-dates-arrival').firstChild;
+  return validate(checkInTag.getAttribute('data-date'));
 }
 
 function getCheckOutDate() {
-    let checkOutTag = document.getElementById("fb-qs-summary-dates-departure").firstChild;
-    return validate(checkOutTag.getAttribute("data-date"))
+  const checkOutTag = document.getElementById('fb-qs-summary-dates-departure').firstChild;
+  return validate(checkOutTag.getAttribute('data-date'));
 }
 
 function getMinimumPrice() {
-    let results = document.getElementsByClassName("fb-results-accommodation")
-    let lowestPrice = 0;
-    
-    Array.from(results).forEach( (room, index, array) => {
-        let price = room.querySelector(".new-price").firstChild.getAttribute("data-price");
-        if(index === 0 || parseFloat(price) < parseFloat(lowestPrice)) {
-            lowestPrice = price;
-        }
-    })
-    return Math.round(lowestPrice * 100) / 100;
+  const results = document.getElementsByClassName('fb-results-accommodation');
+  let lowestPrice = 0;
+
+  Array.from(results).forEach((room, index) => {
+    const price = room.querySelector('.new-price').firstChild.getAttribute('data-price');
+    if (index === 0 || parseFloat(price) < parseFloat(lowestPrice)) {
+      lowestPrice = price;
+    }
+  });
+  return Math.round(lowestPrice * 100) / 100;
 }
 
 function getMemberPrice() {
-    let memberPriceTag = document.querySelector(".btn--member-price")
-    let price = memberPriceTag.querySelector(".fb-price").getAttribute("data-price")
+  const memberPriceTag = document.querySelector('.btn--member-price');
+  const price = memberPriceTag.querySelector('.fb-price').getAttribute('data-price');
 
-    return validate(Math.round(price * 100) / 100);
+  return validate(Math.round(price * 100) / 100);
 }
 
 function getCurrency() {
-    let currency = document.getElementById("fb-headbar-block-currency").querySelector(".fb-headbar-value").textContent;
-    let currencyCode = currency.substring(
-        currency.indexOf( "(" ) +1, currency.indexOf( ")" ) 
-    )
-    return validate(currencyCode);
+  const currency = document.getElementById('fb-headbar-block-currency').querySelector('.fb-headbar-value').textContent;
+  const currencyCode = currency.substring(
+    currency.indexOf('(') + 1, currency.indexOf(')'),
+  );
+  return validate(currencyCode);
 }
 
 function getNumberOfRooms() {
-    let roomTag = document.getElementById("fb-qs-summary-rooms-quantity").firstChild;
-    return validate(+ roomTag.getAttribute("data-mode"))
+  const roomTag = document.getElementById('fb-qs-summary-rooms-quantity').firstChild;
+  return validate(+roomTag.getAttribute('data-mode'));
 }
 
 function getNumberOfGuests() {
-    let adultsTag = document.getElementById("fb-qs-summary-rooms-adults").firstChild;
-    let childrenTag = document.getElementById("fb-qs-summary-rooms-children").querySelector("[data-key='child']");
-    
-    return validate({
-        adults: + adultsTag.getAttribute("data-mode"),
-        children: + childrenTag.getAttribute("data-mode")
-    })
+  const adultsTag = document.getElementById('fb-qs-summary-rooms-adults').firstChild;
+  const childrenTag = document.getElementById('fb-qs-summary-rooms-children').querySelector("[data-key='child']");
+
+  return validate({
+    adults: +adultsTag.getAttribute('data-mode'),
+    children: +childrenTag.getAttribute('data-mode'),
+  });
 }
 
 function getTotalAmountOfGuests() {
-    let guests = getNumberOfGuests()
-    return validate(guests.adults + guests.children)
+  const guests = getNumberOfGuests();
+  return validate(guests.adults + guests.children);
 }
 
 function getLanguageCode() {
-    let lang = document.firstElementChild.getAttribute("lang")
-    return validate(lang.toUpperCase());
+  const lang = document.firstElementChild.getAttribute('lang');
+  return validate(lang.toUpperCase());
 }
 
 function getRoomDetails() {
-    // The text returned is in the language of the search
-    let breakfast = document.querySelector("[data-key='results-rate-meal-type-breakfast']").textContent
-    let refundable = document.querySelector("[data-key='warrant-cancellable-amendable']").textContent
-    let payment = document.querySelector("[data-key='results-rate-payment-hotel']").textContent
-    
-    return validate({
-        breakfast: breakfast,
-        refundPolicy: refundable,
-        payment: payment,
-    })
+  // The text returned is in the language of the search
+  const breakfast = document.querySelector("[data-key='results-rate-meal-type-breakfast']").textContent;
+  const refundable = document.querySelector("[data-key='warrant-cancellable-amendable']").textContent;
+  const payment = document.querySelector("[data-key='results-rate-payment-hotel']").textContent;
+
+  return validate({
+    breakfast,
+    refundPolicy: refundable,
+    payment,
+  });
 }
 
 function getAllRoomsRates() {
-    let results = document.getElementsByClassName("fb-results-accommodation")
-    let roomsRates = []
-    
-    Array.from(results).forEach( (room, index, array) => {
-        let roomInfo = {}, roomDetails = {}
-        let price = + room.querySelector(".new-price").firstChild.getAttribute("data-price");
-        
-        roomInfo.price = Math.round(price * 100) / 100;
-        roomDetails.breakfast = room.querySelector("[data-key='results-rate-meal-type-breakfast']").textContent
-        roomDetails.refundPolicy = room.querySelector("[data-key='warrant-cancellable-amendable']").textContent
-        roomDetails.payment = room.querySelector("[data-key='results-rate-payment-hotel']").textContent
-        roomInfo.roomDetails = roomDetails
+  const results = document.getElementsByClassName('fb-results-accommodation');
+  const roomsRates = [];
 
-        roomsRates.push(roomInfo);
-    })
-    return validate(roomsRates);
+  Array.from(results).forEach((room) => {
+    const roomInfo = {}; const
+      roomDetails = {};
+    const price = +room.querySelector('.new-price').firstChild.getAttribute('data-price');
+
+    roomInfo.price = Math.round(price * 100) / 100;
+    roomDetails.breakfast = room.querySelector("[data-key='results-rate-meal-type-breakfast']").textContent;
+    roomDetails.refundPolicy = room.querySelector("[data-key='warrant-cancellable-amendable']").textContent;
+    roomDetails.payment = room.querySelector("[data-key='results-rate-payment-hotel']").textContent;
+    roomInfo.roomDetails = roomDetails;
+
+    roomsRates.push(roomInfo);
+  });
+  return validate(roomsRates);
 }
 
 function validate(data) {
-    if(!data) {
-        throw new Error("DATA NOT FOUND")
-    }
-    return data;
+  if (!data) {
+    throw new Error('DATA NOT FOUND');
+  }
+  return data;
 }
 
-module.exports = getSearchData
+module.exports = getSearchData;
